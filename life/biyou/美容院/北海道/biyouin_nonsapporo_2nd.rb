b1b8191d1d8393175b0a597dfcd9ed8f103b0fe6
timakin <timaki.st@gmail.com>
2013-06-14 17:18:27 +0900

    first

---
+++ life/biyou/美容院/北海道/biyouin_nonsapporo_2nd.rb
@@ -0,0 +1,55 @@
+# encoding: utf-8
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'timeout'
+require 'csv'
+require 'nkf'
+
+(10..15).each do |hoge|
+  p "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#{hoge}"
+(0..99).each do |i|
+  p "==========================================#{i}"
+  url = "http://search.loco.yahoo.co.jp/search?areacd=012#{hoge}&genrecd=0419001&property=&ei=UTF-8&xargs=#{i+1}&b=#{i}1"
+  doc=Nokogiri::HTML(open(url).read)
+  CSV.open("biyouin_nonsapporo_2nd.csv", "a") do |csv| # CSVファイルを生成
+      if doc.xpath('//*[@id="S1ak"]/p/em').inner_text.include?('条件に一致する検索結果はありません。')
+        p 'not found' # 施設情報が無いページの場合
+      else
+        doc.css('div.tl > h3 > a').each do |item|
+          url2=item[:href]
+          p "行き先：" + url2
+          doc2=Nokogiri::HTML(open(url2).read)
+              #title
+              doc2.css('div.ttl > h1 > a').each do |elem|
+                $s = elem.text # 施設名を取得
+                p "タイトル：#{$s}"
+                #csv << [s]
+              end
+
+              #url
+              doc2.css('p.c > a').each do |elem|
+                if /^http:/ =~ elem[:href] # URLの書式に合致する場合
+                  $u = elem[:href]
+                  #csv << [u]
+                  p "url:#{$u}"
+                end
+              end
+
+              #email
+              doc2.css('p.c').each do |elem| # email or url を取得
+                test = elem.children.inner_text.rstrip.gsub(/(\s|　)+/, '')
+                if /^[0-9A-Za-z\._-]+@[0-9A-Za-z\._-]+[a-z]/ =~ test # emailの書式に合致する場合
+                  $e = test.gsub(/[^\x01-\x7E]|[ァ-ー]|[ぁ-ヶ]|[亜-黑]/, '')
+                  #csv << [e]
+                  p "email:#{$e}"
+                  csv << [$s, $e, $u]
+                end
+              end
+              $e, $u = nil, nil
+        end
+      end
+  end
+end
+end
+
