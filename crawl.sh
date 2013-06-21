bcb3eefd3d3159b4f7e7766843cc9ec0ebdf6406
timakin <timaki.st@gmail.com>
2013-06-21 12:04:07 +0900

    sh

--- crawl.sh
+++ crawl.sh
@@ -3,190 +3,190 @@
 
 ##########food
 #bar
-nohup ruby food/bar/tokyo/tokyo_2nd.rb
-nohup ruby food/bar/tokyo/tokyo_3rd.rb
-nohup ruby food/bar/tokyo/tokyo_4th.rb
-nohup ruby food/bar/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb
-nohup ruby food/bar/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb
-nohup ruby food/bar/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb
-nohup ruby food/bar/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb
-nohup ruby food/bar/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb
-nohup ruby food/bar/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb
-nohup ruby food/bar/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb
-nohup ruby food/bar/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb
+nohup ruby food/bar/tokyo/tokyo_2nd.rb &
+nohup ruby food/bar/tokyo/tokyo_3rd.rb &
+nohup ruby food/bar/tokyo/tokyo_4th.rb &
+nohup ruby food/bar/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb &
+nohup ruby food/bar/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb &
+nohup ruby food/bar/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb &
+nohup ruby food/bar/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb &
+nohup ruby food/bar/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb &
+nohup ruby food/bar/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb &
+nohup ruby food/bar/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb &
+nohup ruby food/bar/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb &
 
 #china
-nohup ruby food/china/tokyo/tokyo_1st.rb
-nohup ruby food/china/tokyo/tokyo_2nd.rb
-nohup ruby food/china/tokyo/tokyo_3rd.rb
-nohup ruby food/china/tokyo/tokyo_4th.rb
-nohup ruby food/china/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb
-nohup ruby food/china/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb
-nohup ruby food/china/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb
-nohup ruby food/china/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb
-nohup ruby food/china/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb
-nohup ruby food/china/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb
-nohup ruby food/china/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb
-nohup ruby food/china/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb
+nohup ruby food/china/tokyo/tokyo_1st.rb &
+nohup ruby food/china/tokyo/tokyo_2nd.rb &
+nohup ruby food/china/tokyo/tokyo_3rd.rb &
+nohup ruby food/china/tokyo/tokyo_4th.rb &
+nohup ruby food/china/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb &
+nohup ruby food/china/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb &
+nohup ruby food/china/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb &
+nohup ruby food/china/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb &
+nohup ruby food/china/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb &
+nohup ruby food/china/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb &
+nohup ruby food/china/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb &
+nohup ruby food/china/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb &
 
 #washoku
-nohup ruby food/wasyoku/tokyo/tokyo_1st.rb
-nohup ruby food/wasyoku/tokyo/tokyo_2nd.rb
-nohup ruby food/wasyoku/tokyo/tokyo_3rd.rb
-nohup ruby food/wasyoku/tokyo/tokyo_4th.rb
-nohup ruby food/wasyoku/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb
-nohup ruby food/wasyoku/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb
-nohup ruby food/wasyoku/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb
-nohup ruby food/wasyoku/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb
-nohup ruby food/wasyoku/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb
-nohup ruby food/wasyoku/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb
-nohup ruby food/wasyoku/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb
-nohup ruby food/wasyoku/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb
+nohup ruby food/wasyoku/tokyo/tokyo_1st.rb &
+nohup ruby food/wasyoku/tokyo/tokyo_2nd.rb &
+nohup ruby food/wasyoku/tokyo/tokyo_3rd.rb &
+nohup ruby food/wasyoku/tokyo/tokyo_4th.rb &
+nohup ruby food/wasyoku/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb &
+nohup ruby food/wasyoku/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb &
+nohup ruby food/wasyoku/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb &
+nohup ruby food/wasyoku/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb &
+nohup ruby food/wasyoku/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb &
+nohup ruby food/wasyoku/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb &
+nohup ruby food/wasyoku/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb &
+nohup ruby food/wasyoku/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb &
 
 #yosyoku
-nohup ruby food/yosyoku/tokyo/tokyo_1st.rb
-nohup ruby food/yosyoku/tokyo/tokyo_2nd.rb
-nohup ruby food/yosyoku/tokyo/tokyo_3rd.rb
-nohup ruby food/yosyoku/tokyo/tokyo_4th.rb
-nohup ruby food/yosyoku/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb
-nohup ruby food/yosyoku/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb
-nohup ruby food/yosyoku/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb
-nohup ruby food/yosyoku/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb
-nohup ruby food/yosyoku/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb
-nohup ruby food/yosyoku/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb
-nohup ruby food/yosyoku/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb
-nohup ruby food/yosyoku/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb
+nohup ruby food/yosyoku/tokyo/tokyo_1st.rb &
+nohup ruby food/yosyoku/tokyo/tokyo_2nd.rb &
+nohup ruby food/yosyoku/tokyo/tokyo_3rd.rb &
+nohup ruby food/yosyoku/tokyo/tokyo_4th.rb &
+nohup ruby food/yosyoku/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb &
+nohup ruby food/yosyoku/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb &
+nohup ruby food/yosyoku/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb &
+nohup ruby food/yosyoku/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb &
+nohup ruby food/yosyoku/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb &
+nohup ruby food/yosyoku/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb &
+nohup ruby food/yosyoku/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb &
+nohup ruby food/yosyoku/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb &
 
 
 #########leisure
 #hotel
-nohup ruby leisure/hotel/tokyo/tokyo_1st.rb
-nohup ruby leisure/hotel/tokyo/tokyo_2nd.rb
-nohup ruby leisure/hotel/tokyo/tokyo_3rd.rb
-nohup ruby leisure/hotel/tokyo/tokyo_4th.rb
-nohup ruby leisure/hotel/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb
-nohup ruby leisure/hotel/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb
-nohup ruby leisure/hotel/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb
-nohup ruby leisure/hotel/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb
-nohup ruby leisure/hotel/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb
-nohup ruby leisure/hotel/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb
-nohup ruby leisure/hotel/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb
-nohup ruby leisure/hotel/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb
+nohup ruby leisure/hotel/tokyo/tokyo_1st.rb &
+nohup ruby leisure/hotel/tokyo/tokyo_2nd.rb &
+nohup ruby leisure/hotel/tokyo/tokyo_3rd.rb &
+nohup ruby leisure/hotel/tokyo/tokyo_4th.rb &
+nohup ruby leisure/hotel/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb &
+nohup ruby leisure/hotel/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb &
+nohup ruby leisure/hotel/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb &
+nohup ruby leisure/hotel/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb &
+nohup ruby leisure/hotel/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb &
+nohup ruby leisure/hotel/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb &
+nohup ruby leisure/hotel/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb &
+nohup ruby leisure/hotel/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb &
 
 #leisure
-nohup ruby leisure/leisure/tokyo/tokyo_1st.rb
-nohup ruby leisure/leisure/tokyo/tokyo_2nd.rb
-nohup ruby leisure/leisure/tokyo/tokyo_3rd.rb
-nohup ruby leisure/leisure/tokyo/tokyo_4th.rb
-nohup ruby leisure/leisure/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb
-nohup ruby leisure/leisure/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb
-nohup ruby leisure/leisure/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb
-nohup ruby leisure/leisure/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb
-nohup ruby leisure/leisure/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb
-nohup ruby leisure/leisure/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb
-nohup ruby leisure/leisure/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb
-nohup ruby leisure/leisure/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb
+nohup ruby leisure/leisure/tokyo/tokyo_1st.rb &
+nohup ruby leisure/leisure/tokyo/tokyo_2nd.rb &
+nohup ruby leisure/leisure/tokyo/tokyo_3rd.rb &
+nohup ruby leisure/leisure/tokyo/tokyo_4th.rb &
+nohup ruby leisure/leisure/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb &
+nohup ruby leisure/leisure/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb &
+nohup ruby leisure/leisure/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb &
+nohup ruby leisure/leisure/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb &
+nohup ruby leisure/leisure/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb &
+nohup ruby leisure/leisure/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb &
+nohup ruby leisure/leisure/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb &
+nohup ruby leisure/leisure/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb &
 
 ##########life
 #biyou
-nohup ruby life/biyou/tokyo/tokyo_1st.rb
-nohup ruby life/biyou/tokyo/tokyo_2nd.rb
-nohup ruby life/biyou/tokyo/tokyo_3rd.rb
-nohup ruby life/biyou/tokyo/tokyo_4th.rb
-nohup ruby life/biyou/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb
-nohup ruby life/biyou/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb
-nohup ruby life/biyou/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb
-nohup ruby life/biyou/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb
-nohup ruby life/biyou/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb
-nohup ruby life/biyou/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb
-nohup ruby life/biyou/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb
-nohup ruby life/biyou/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb
+nohup ruby life/biyou/tokyo/tokyo_1st.rb &
+nohup ruby life/biyou/tokyo/tokyo_2nd.rb &
+nohup ruby life/biyou/tokyo/tokyo_3rd.rb &
+nohup ruby life/biyou/tokyo/tokyo_4th.rb &
+nohup ruby life/biyou/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb &
+nohup ruby life/biyou/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb &
+nohup ruby life/biyou/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb &
+nohup ruby life/biyou/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb &
+nohup ruby life/biyou/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb &
+nohup ruby life/biyou/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb &
+nohup ruby life/biyou/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb &
+nohup ruby life/biyou/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb &
 
 #life_service
-nohup ruby life/life_service/tokyo/tokyo_1st.rb
-nohup ruby life/life_service/tokyo/tokyo_2nd.rb
-nohup ruby life/life_service/tokyo/tokyo_3rd.rb
-nohup ruby life/life_service/tokyo/tokyo_4th.rb
-nohup ruby life/life_service/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb
-nohup ruby life/life_service/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb
-nohup ruby life/life_service/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb
-nohup ruby life/life_service/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb
-nohup ruby life/life_service/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb
-nohup ruby life/life_service/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb
-nohup ruby life/life_service/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb
-nohup ruby life/life_service/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb
+nohup ruby life/life_service/tokyo/tokyo_1st.rb &
+nohup ruby life/life_service/tokyo/tokyo_2nd.rb &
+nohup ruby life/life_service/tokyo/tokyo_3rd.rb &
+nohup ruby life/life_service/tokyo/tokyo_4th.rb &
+nohup ruby life/life_service/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb &
+nohup ruby life/life_service/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb &
+nohup ruby life/life_service/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb &
+nohup ruby life/life_service/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb &
+nohup ruby life/life_service/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb &
+nohup ruby life/life_service/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb &
+nohup ruby life/life_service/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb &
+nohup ruby life/life_service/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb &
 
 #medical
-nohup ruby life/medical/tokyo/tokyo_1st.rb
-nohup ruby life/medical/tokyo/tokyo_2nd.rb
-nohup ruby life/medical/tokyo/tokyo_3rd.rb
-nohup ruby life/medical/tokyo/tokyo_4th.rb
-nohup ruby life/medical/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb
-nohup ruby life/medical/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb
-nohup ruby life/medical/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb
-nohup ruby life/medical/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb
-nohup ruby life/medical/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb
-nohup ruby life/medical/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb
-nohup ruby life/medical/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb
-nohup ruby life/medical/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb
+nohup ruby life/medical/tokyo/tokyo_1st.rb &
+nohup ruby life/medical/tokyo/tokyo_2nd.rb &
+nohup ruby life/medical/tokyo/tokyo_3rd.rb &
+nohup ruby life/medical/tokyo/tokyo_4th.rb &
+nohup ruby life/medical/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb &
+nohup ruby life/medical/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb &
+nohup ruby life/medical/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb &
+nohup ruby life/medical/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb &
+nohup ruby life/medical/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb &
+nohup ruby life/medical/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb &
+nohup ruby life/medical/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb &
+nohup ruby life/medical/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb &
 
 #syumi
-nohup ruby life/syumi/tokyo/tokyo_1st.rb
-nohup ruby life/syumi/tokyo/tokyo_2nd.rb
-nohup ruby life/syumi/tokyo/tokyo_3rd.rb
-nohup ruby life/syumi/tokyo/tokyo_4th.rb
-nohup ruby life/syumi/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb
-nohup ruby life/syumi/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb
-nohup ruby life/syumi/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb
-nohup ruby life/syumi/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb
-nohup ruby life/syumi/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb
-nohup ruby life/syumi/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb
-nohup ruby life/syumi/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb
-nohup ruby life/syumi/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb
+nohup ruby life/syumi/tokyo/tokyo_1st.rb &
+nohup ruby life/syumi/tokyo/tokyo_2nd.rb &
+nohup ruby life/syumi/tokyo/tokyo_3rd.rb &
+nohup ruby life/syumi/tokyo/tokyo_4th.rb &
+nohup ruby life/syumi/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb &
+nohup ruby life/syumi/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb &
+nohup ruby life/syumi/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb &
+nohup ruby life/syumi/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb &
+nohup ruby life/syumi/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb &
+nohup ruby life/syumi/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb &
+nohup ruby life/syumi/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb &
+nohup ruby life/syumi/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb &
 
 
 #########shopping
 #fashion
-nohup ruby shopping/fashion/tokyo/tokyo_1st.rb
-nohup ruby shopping/fashion/tokyo/tokyo_2nd.rb
-nohup ruby shopping/fashion/tokyo/tokyo_3rd.rb
-nohup ruby shopping/fashion/tokyo/tokyo_4th.rb
-nohup ruby shopping/fashion/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb
-nohup ruby shopping/fashion/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb
-nohup ruby shopping/fashion/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb
-nohup ruby shopping/fashion/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb
-nohup ruby shopping/fashion/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb
-nohup ruby shopping/fashion/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb
-nohup ruby shopping/fashion/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb
-nohup ruby shopping/fashion/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb
+nohup ruby shopping/fashion/tokyo/tokyo_1st.rb &
+nohup ruby shopping/fashion/tokyo/tokyo_2nd.rb &
+nohup ruby shopping/fashion/tokyo/tokyo_3rd.rb &
+nohup ruby shopping/fashion/tokyo/tokyo_4th.rb &
+nohup ruby shopping/fashion/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb &
+nohup ruby shopping/fashion/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb &
+nohup ruby shopping/fashion/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb &
+nohup ruby shopping/fashion/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb &
+nohup ruby shopping/fashion/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb &
+nohup ruby shopping/fashion/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb &
+nohup ruby shopping/fashion/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb &
+nohup ruby shopping/fashion/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb &
 
 #food
-nohup ruby shopping/food/tokyo/tokyo_1st.rb
-nohup ruby shopping/food/tokyo/tokyo_2nd.rb
-nohup ruby shopping/food/tokyo/tokyo_3rd.rb
-nohup ruby shopping/food/tokyo/tokyo_4th.rb
-nohup ruby shopping/food/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb
-nohup ruby shopping/food/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb
-nohup ruby shopping/food/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb
-nohup ruby shopping/food/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb
-nohup ruby shopping/food/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb
-nohup ruby shopping/food/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb
-nohup ruby shopping/food/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb
-nohup ruby shopping/food/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb
+nohup ruby shopping/food/tokyo/tokyo_1st.rb &
+nohup ruby shopping/food/tokyo/tokyo_2nd.rb &
+nohup ruby shopping/food/tokyo/tokyo_3rd.rb &
+nohup ruby shopping/food/tokyo/tokyo_4th.rb &
+nohup ruby shopping/food/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb &
+nohup ruby shopping/food/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb &
+nohup ruby shopping/food/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb &
+nohup ruby shopping/food/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb &
+nohup ruby shopping/food/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb &
+nohup ruby shopping/food/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb &
+nohup ruby shopping/food/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb &
+nohup ruby shopping/food/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb &
 
 #zakka
-nohup ruby shopping/zakka/tokyo/tokyo_1st.rb
-nohup ruby shopping/zakka/tokyo/tokyo_2nd.rb
-nohup ruby shopping/zakka/tokyo/tokyo_3rd.rb
-nohup ruby shopping/zakka/tokyo/tokyo_4th.rb
-nohup ruby shopping/zakka/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb
-nohup ruby shopping/zakka/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb
-nohup ruby shopping/zakka/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb
-nohup ruby shopping/zakka/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb
-nohup ruby shopping/zakka/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb
-nohup ruby shopping/zakka/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb
-nohup ruby shopping/zakka/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb
-nohup ruby shopping/zakka/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb
+nohup ruby shopping/zakka/tokyo/tokyo_1st.rb &
+nohup ruby shopping/zakka/tokyo/tokyo_2nd.rb &
+nohup ruby shopping/zakka/tokyo/tokyo_3rd.rb &
+nohup ruby shopping/zakka/tokyo/tokyo_4th.rb &
+nohup ruby shopping/zakka/zenkoku/1st_zenkoku_zenhan_zenhan_zenhan.rb &
+nohup ruby shopping/zakka/zenkoku/2nd_zenkoku_zenhan_zenhan_kouhan.rb &
+nohup ruby shopping/zakka/zenkoku/3rd_zenkoku_zenhan_kouhan_zenhan.rb &
+nohup ruby shopping/zakka/zenkoku/4th_zenkoku_zenhan_kouhan_kouhan.rb &
+nohup ruby shopping/zakka/zenkoku/5th_zenkoku_kouhan_zenhan_zenhan.rb &
+nohup ruby shopping/zakka/zenkoku/6th_zenkoku_kouhan_zenhan_kouhan.rb &
+nohup ruby shopping/zakka/zenkoku/7th_zenkoku_kouhan_kouhan_zenhan.rb &
+nohup ruby shopping/zakka/zenkoku/8th_zenkoku_kouhan_kouhan_kouhan.rb &
 
 
 
